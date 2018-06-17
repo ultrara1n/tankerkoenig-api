5ba0ab356273cb9a949fda5413a7305019c42d62
Philipp Wensauer <mail@philippwensauer.com>
2018-06-17 18:27:57 +0200

    Preparements

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tankerkoenig-api",
+    version="0.0.1",
+    author="Philipp Wensauer",
+    author_email="mail@philippwensauer.com",
+    description="Library for Tankerönig.de JSON API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ultrara1n/tankerkoenig-api",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+    ),
+)
---
+++ tankerkoenig-api/__init__.py
@@ -0,0 +1,1 @@
+name = "tankerkoenig-api"
