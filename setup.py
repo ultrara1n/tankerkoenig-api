b31a71aa401ac0807eb47084d8cc77e250697e0a
Philipp Wensauer <mail@philippwensauer.com>
2018-06-18 14:58:18 +0200

    changed name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tankerkoenig-api",
+    name="pytankerkoenig",
     version="0.0.1",
     author="Philipp Wensauer",
     author_email="mail@philippwensauer.com",
