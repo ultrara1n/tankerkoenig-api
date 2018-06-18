1e5a9086f5d4de8309528e29cb2234037600c4a8
Philipp Wensauer <mail@philippwensauer.com>
2018-06-18 21:36:50 +0200

    real testing, whoop whoop

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import pytankerkoenig
 import os
 
-print(os.environ['AAASD'])
+print(pytankerkoenig.getStationData(os.environ['api_key'],'5bd71e9d-7001-4908-a29d-36c28d6eb615'))
+
+print(pytankerkoenig.getNearbyStations(os.environ['api_key'],'50.758914','7.201223','5','all','dist'))
