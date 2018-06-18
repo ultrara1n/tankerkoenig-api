08fd910c28c1c709f60997240e4f8c0738b2908f
Philipp Wensauer <mail@philippwensauer.com>
2018-06-18 20:53:59 +0200

    add nearby stations api

--- pytankerkoenig/__init__.py
+++ pytankerkoenig/__init__.py
@@ -16,3 +16,15 @@ def getStationData(apikey, station):
     stationJSON = json.loads(stationResponse.read().decode(encoding))
 
     return stationJSON
+
+def getNearbyStations(apikey, latitude, longitude, radius, type, sort):
+    # Build the URL with apikey and station-id to fetch station details
+    stationBaseurl = "https://creativecommons.tankerkoenig.de/json/list.php?lat="
+    stationFullurl = stationBaseurl + latitude + "&lng=" + longitude + "&rad=" + radius + "&sort=" + sort + "&type=" + type + "&apikey=" + apikey
+    # Fetch station details
+    stationResponse = request.urlopen(stationFullurl)
+    # Prepare the response
+    encoding = stationResponse.headers['content-type'].split('charset=')[-1]
+    nearbyStationsJSON = json.loads(stationResponse.read().decode(encoding))
+
+    return nearbyStationsJSON
