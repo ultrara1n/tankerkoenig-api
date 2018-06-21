2e3a48243b16533d37dcad42b1ecd55fa7a49d98
Philipp Wensauer <mail@philippwensauer.com>
2018-06-21 14:55:31 +0200

    bump to 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytankerkoenig",
-    version="0.0.5",
+    version="0.0.6",
     author="Philipp Wensauer",
     author_email="mail@philippwensauer.com",
     description="Library for Tankerkoenig.de JSON API",
@@ -14,7 +14,12 @@ setuptools.setup(
     url="https://github.com/ultrara1n/tankerkoenig-api",
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
